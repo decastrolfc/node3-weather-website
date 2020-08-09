7f6875dce0bb96553ded53420be6dc2efa512e4c
decastrolfc <decastrolfc@gmail.com>
2020-08-09 13:40:42 +0100

    Adding Wind Speed information

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,13 +1,3 @@
-//
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
-
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) =>{
@@ -18,7 +8,7 @@ const forecast = (latitude, longitude, callback) =>{
     } else if(body.error) {
         callback('Unable to find location', undefined)
     } else{
-        callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+        callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. Wind speed is ' + body.current.wind_speed)
     }
 })
 }
